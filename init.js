9581d13e8857d45e771340c7c5a1eb118837c6e9
Marcus Jang <makerpopo@gmail.com>
2022-01-10 19:14:59 +0900

    Refactor init.js to utilise promise queue while removing cron

--- init.js
+++ init.js
@@ -6,12 +6,41 @@
 const debug = require('debug')('sir-ardbot:main');
 const path = require('path');
 const fs = require('fs/promises');
-const { CronJob } = require('cron');
 
 const crawl = require('./crawl.js');
 const discord = require('./discord.js');
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const queue = {
+	array: [],
+	arrayCopy: [],
+	pending: false,
+	working: false,
+	unitDelay: 2500 // the base amount of time in between jobs
+					// used to not clog up the http req, will be overwritten depending on the job size
+}
+
+const period = 90 * 1000; // theorically each site should be crawled every 90 seconds
+
+const work = () => {
+	if (queue.working) return false;
+	const job = queue.array.shift();
+	if (!job) return false;
+	if (queue.array.length == 0) queue.array = queue.arrayCopy.slice(); // until the end of time (or usually SIGINT)
+	queue.working = true;
+	// some .race() and .all() to do job not too fast but not too slow
+	Promise.race([
+			Promise.all([ job(), delay(queue.unitDelay) ]),
+			delay(queue.unitDelay + 3000) // basically timeout
+		])
+		// we do not care if crawl() properly resolves or not so just use .finally()
+		.finally(() => { 
+			queue.working = false;
+			work();
+		});
+	return true;
+};
 
 const init = async () => {
 	await require('./database.js').init(); // initialise the database
@@ -23,19 +52,15 @@ const init = async () => {
 		.then(files => files.filter(file => (file.charAt(0) != '_' && file.endsWith('.js'))))
 		.then(files => discord.initChannels(files))
 		.then(channelArray => {
-			const unitDelay = Math.floor((60 * 1000) / channelArray.length); // 60 seconds = 1 minute
-			debug(`unit delay is ${unitDelay}ms per site`);
+			queue.unitDelay = Math.floor(period / channelArray.length + Math.random()*500); // 60 seconds = 1 minute
+			debug(`unit delay is ${queue.unitDelay}ms per site`);
 
-			// one cronjob for ~~site each~~ all sites and the delay will be given with an await()
-			debug(`Assigning cronjob in place`);
-			new CronJob(`*/2 * * * *`, () => {
-				channelArray.forEach(async (channelObj, index) => {
-					await delay(unitDelay * index + Math.floor(Math.random()*500)); // add some random delay
-					const { site, channel } = channelObj;
-					debug(`Begin crawling for ${site}!`);
-					crawl(site).then(products => discord.sendProducts(channel, products));
-				});
-			}, null, true);
+			queue.array = channelArray.map(channelObj => {
+				const { site, channel } = channelObj;
+				return () => crawl(site).then(products => discord.sendProducts(channel, products));
+			});
+			queue.arrayCopy = queue.array.map(el => el); // just to copy the array
+			work();
 		});
 };
 
