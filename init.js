b30bc54e1310b1e714a12086709a4e5d6b0237d4
Marcus Jang <makerpopo@gmail.com>
2022-01-21 02:08:11 +0900

    Update initialisation routine further

--- init.js
+++ init.js
@@ -2,6 +2,7 @@ import { readdir } from 'fs/promises';
 import puppeteer from 'puppeteer';
 import config from './config.js';
 import * as discord from './discord.js';
+import * as database from './database.js';
 import debug, { print } from './utils/debug.js';
 import PathURL from './classes/pathurl.js';
 
@@ -10,6 +11,7 @@ const log = debug('sir-ardbot:init');
 export default function() {
 	log('Sir Ardbot is initialising...');
 	readdir(new PathURL('sites').path)
+		// get site modules then import them
 		.then(files => files.filter(file => (file.charAt(0) != '_' && file.endsWith('.js'))))
 		.then(sites => {
 			if (sites.length === 0) {
@@ -22,31 +24,34 @@ export default function() {
 				sites = [ '_example.js' ];
 			}
 
-
 			const importation = () => {
-				return sites.map(site => import(new PathURL(`sites/${site}`).href));
+				return sites.map(site => {
+					return import(new PathURL(`sites/${site}`).href);
+				});
 			}
 
-			return Promise.all(importation());
-		})
-		.then(sites => {
-			sites = sites.map(site => site.default)
 			log('Found %d site module(s)...', sites.length);
-			return new Promise(resolve => {
-				if (!config.discord.disabled) {
-					discord.login()
-						.then(() => discord.initChannels(sites))
-						.then(resolve);
-				} else {
-					resolve(sites);
-				}
-			});
+
+			return Promise.all(importation())
+				.then(sites => sites.map(site => site.default));
 		})
+
+		// login on discord if necessary then modularise the site modules
 		.then(sites => {
-			//print(sites);
+			if (config.discord.disabled)
+				return sites;
+
+			return discord.login()
+				.then(() => discord.initChannels(sites));
+		})
+
+		// launch puppeteer and initialise the database
+		.then(sites => {
+			Promise.all([ puppeteer.launch(config.puppeteer.options), database.init() ])
+				.then(init => {
+					const [ browser ] = init;
+					// TODO init further
+				});
 		});
 
-	//puppeteer.launch()
-	//	.then(browser => console.log(browser));
-	// TODO init
 }
