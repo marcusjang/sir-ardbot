34a066df1821de3e91e85a84f79e315551f1b441
Marcus Jang <makerpopo@gmail.com>
2023-01-27 11:25:10 +0900

    Change index.js to use debug module

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import config from './config.js';
 import { init } from './main.js';
 import { client } from './discord.js';
+import { debug } from './utils.js';
 
-function info(message, ...args) {
-	return console.info('  \x1b[33msir-ardbot\x1b[0m ' + message, ...args);
-}
+const log = debug('sir-ardbot');
 
 function exitHandler(code) {
-	info('Process event %s was received; exiting...', code);
+	log('Process event %s was received; exiting...', code);
 	if (client.readyAt !== null)
 		client.destroy();
 	process.exit(0);
@@ -16,5 +15,5 @@ function exitHandler(code) {
 process.on('SIGINT', exitHandler);
 process.on('SIGTERM', exitHandler);
 
-info('Sir Ardbot is initialising...');
+log('Sir Ardbot is initialising...');
 init();
