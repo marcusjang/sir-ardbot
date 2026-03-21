8a5ff1863592bb3109d92657cb5693985dafdba3
Marcus Jang <marcus@melange.works>
2026-03-21 23:37:11 +0900

    Revert "Update puppeteer launch args"
    This reverts commit 0474e9c28ebf38d1a3d3dbea8808bf16e9ee2926.

--- config.js
+++ config.js
@@ -35,20 +35,7 @@ const config = {
 		timeout: env.PUPPETEER_TIMEOUT*1 || 10000,
 		path: env.PUPPETEER_PATH || false,
 		console: isTrue(env.PUPPETEER_CONSOLE),
-		options: {
-			args: [
-				'--no-sandbox',
-				'--disable-setuid-sandbox',
-				'--ignore-certificate-errors',
-				'--disable-crash-reporter',
-				'--disable-features=FedCm',
-				'--disable-dev-shm-usage',
-				'--disable-accelerated-2d-canvas',
-				'--no-first-run',
-				'--no-zygote',
-				'--disable-gpu'
-			]
-		}
+		options: { args: [ '--no-sandbox', '--disable-setuid-sandbox', '--ignore-certificate-errors', '--disable-crash-reporter' ] }
 	},
 	debug: {
 		dev: isTrue(env.DEV),
